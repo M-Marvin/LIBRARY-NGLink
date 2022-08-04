36d3e6b2e2f177e2296cb0e7cc484b5dbedf5226
M-Marvin <69386758+M-Marvin@users.noreply.github.com>
2022-08-05 00:12:18 +0200

    NativeExtractor modifiziert, funktioniert jetzt auch mit der .jar
    Ergänzung: Kleine Änderung am Code zur behebung eines Fehler in der Pfadabrage.

--- JNGLink/src/main/java/de/m_marvin/nglink/NativeExtractor.java
+++ JNGLink/src/main/java/de/m_marvin/nglink/NativeExtractor.java
@@ -5,8 +5,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Enumeration;
 import java.util.Scanner;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 public class NativeExtractor {
 	
@@ -20,24 +27,61 @@ public class NativeExtractor {
 		
 		try {
 			System.out.println("Try to load files for " + osPrefix + " os ...");
-			Scanner scanner = new Scanner(NativeExtractor.class.getResourceAsStream("/" + osPrefix));
+
 			boolean failed = false;
-			while (scanner.hasNextLine()) {
-				String fileName = scanner.nextLine();
-				System.out.println("File " + fileName + " ...");
-				URL nativeLib = ClassLoader.getSystemResource(osPrefix + "/" + fileName);
-				if (!extractNative(nativeLib, fileName)) {
-					System.err.println("Failed to extract lib!");
-					failed = true;
-				}
-			}
-			scanner.close();
-			System.out.println(failed ? "Not all natives could be extracted!" : "Done");
 			
+			try {
+
+				String jarLocation = NativeExtractor.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+				if (jarLocation.endsWith("#0!/")) jarLocation = jarLocation.substring(0, jarLocation.length() - 4);
+				if (jarLocation.startsWith("/")) jarLocation = jarLocation.substring(1, jarLocation.length());
+				JarFile libJar = new JarFile(jarLocation);
+				Enumeration<JarEntry> jarEntrys = libJar.entries();
+				while (jarEntrys.hasMoreElements())  {
+					JarEntry entry = jarEntrys.nextElement();
+					if (entry.getName().startsWith(osPrefix + "/") && !entry.isDirectory()) {
+						String[] s = entry.getName().split("/");
+						String libName = s[s.length - 1];
+						System.out.println("File " + libName + " ...");
+						InputStream nativeInputStream = libJar.getInputStream(entry);
+						if (!extractNative(nativeInputStream, libName)) {
+							System.err.println("Failed to extract lib!");
+							failed = true;
+						}
+					}
+				}
+				libJar.close();
+				
+			} catch (FileNotFoundException e) {
+				
+				System.out.println("Could not open jar file, asuming running in ide!");
+
+				Scanner scanner = new Scanner(NativeExtractor.class.getResourceAsStream("/" + osPrefix));
+				while (scanner.hasNextLine()) {
+					String fileName = scanner.nextLine();
+					System.out.println("File " + fileName + " ...");
+					URL nativeLib = ClassLoader.getSystemResource(osPrefix + "/" + fileName);
+					InputStream nativeInputStream = new FileInputStream(new File(nativeLib.getPath()));
+					if (!extractNative(nativeInputStream, fileName)) {
+						System.err.println("Failed to extract lib!");
+						failed = true;
+					}
+				}
+				scanner.close();
+				
+			}
+
+			System.out.println(failed ? "Not all natives could be extracted!" : "Done");
 			NATIVE_TEMP_FOLDER.deleteOnExit();
 			
 			return !failed;
 		} catch (UnsatisfiedLinkError e) {
+			System.err.println("Failed to load native libs for nglink!");
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			System.err.println("Failed to load native libs for nglink!");
+			e.printStackTrace();
+		} catch (IOException e) {
 			System.err.println("Failed to load native libs for nglink!");
 			e.printStackTrace();
 		}
@@ -46,7 +90,7 @@ public class NativeExtractor {
 		
 	}
 	
-	public static boolean extractNative(URL libUrl, String libName) {
+	public static boolean extractNative(InputStream inputStream, String libName) {
 		try {
 			File tempLibFile = new File(NATIVE_TEMP_FOLDER, libName);
 			FileOutputStream outputStream;
@@ -55,8 +99,7 @@ public class NativeExtractor {
 			} catch (FileNotFoundException e) {
 				return true; // File exists already and is in use
 			}
-			FileInputStream inputStream = new FileInputStream(new File(libUrl.getPath()));
-			outputStream.getChannel().transferFrom(inputStream.getChannel(), 0, inputStream.getChannel().size());
+			outputStream.write(inputStream.readAllBytes());
 			outputStream.close();
 			inputStream.close();
 			return true;
