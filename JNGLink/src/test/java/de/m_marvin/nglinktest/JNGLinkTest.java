88c1027684ae5830838b2cfd0ff4ab61e1eff1b6
M-Marvin <69386758+M-Marvin@users.noreply.github.com>
2024-03-10 20:29:17 +0100

    Updated to ngspice 42

--- JNGLink/src/test/java/de/m_marvin/nglinktest/JNGLinkTest.java
+++ JNGLink/src/test/java/de/m_marvin/nglinktest/JNGLinkTest.java
@@ -1,8 +1,6 @@
 package de.m_marvin.nglinktest;
 
-import java.util.Scanner;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.io.File;
 
 import de.m_marvin.nglink.NativeNGLink;
 import de.m_marvin.nglink.NativeNGLink.INGCallback;
@@ -18,6 +16,15 @@ import de.m_marvin.nglink.NativeNGLink.VectorValuesAll;
 public class JNGLinkTest {
 	
 	public static void main(String... args) throws InterruptedException {
+		
+		File runDir = new File(new File("").getAbsolutePath());
+		File initFile = new File(runDir, "..\\share\\ngspice\\scripts\\spinit");
+		
+		initFile.getParentFile().mkdirs();
+		
+		System.out.println(initFile);
+		
+		//System.exit(0);
 		
 //			try {
 //				test();
@@ -105,18 +112,69 @@ public class JNGLinkTest {
 	
 	public static void test1(NativeNGLink nglink) {
 		synchronized (ngLinkLock) {
-
+			
+			
+			//nglink.initNGSpice("E:\\GitHub\\LIBRARY-NGLink\\JNGLink\\src\\main\\resources\\natives\\libngspice-0.dll");
+			//nglink.initNGSpice("E:\\GitHub\\LIBRARY-NGLink\\JNGLink\\src\\main\\resources\\natives\\ngspice42_x64.dll");
 			nglink.initNGSpice();
 			
-			nglink.loadCircuit("test circuit\r\n"
-					+ "V1 in gnd 10\n"
-					+ "R1 in out 1k\n"
-					+ "R2 out gnd 1k\n"
-					+ ".end\n"
-					+ ".tran 1 10\n"
-					+ "");
+//			nglink.loadCircuit("test circuit\r\n"
+//					+ "V1 in gnd 10\n"
+//					+ "R1 in out 1k\n"
+//					+ "R2 out gnd 1k\n"
+//					+ ".end\n"
+//					+ ".tran 1 10\n"
+//					+ "");
 			
-			nglink.execCommand("tran 1 1");
+			nglink.execCommand("version -f");
+			String c = "ingame-level-circuit\r\n"
+					+ "* Component Block{industria:portable_fuel_generator} BlockPos{x=6, y=-60, z=-13}\r\n"
+					+ "* Current limited power source [ i_nominal=0.0, v_nominal=0 ]\r\n"
+					+ "R1_0 node[pos6_-60_-13_id0_lid2_lnmpower_shunt] node[pos6_-60_-13_id0_lid0_lnml] 0.001\r\n"
+					+ "I1_0 N003_0 N002_0 0.0\r\n"
+					+ "V1_0 N001_0 N002_0 0\r\n"
+					+ "I2_0 N003_0 node[pos6_-60_-13_id0_lid1_lnmn] 0.0\r\n"
+					+ "*S4_0 node[pos6_-60_-13_id0_lid2_lnmpower_shunt] N001_0 N001_0 node[pos6_-60_-13_id0_lid2_lnmpower_shunt] source_diosw\r\n"
+					+ "*S5_0 N003_0 N002_0 N002_0 N003_0 source_diosw\r\n"
+					+ "*S6_0 N003_0 node[pos6_-60_-13_id0_lid1_lnmn] node[pos6_-60_-13_id0_lid1_lnmn] N003_0 source_diosw\r\n"
+					+ "\r\n"
+					+ "A1_0 N001_0 node ds1\r\n"
+					+ "A2_0 N002_0 N003_0 ds1\r\n"
+					+ ".model DS1 sidiode(Roff=1000 Ron=0.7 Rrev=0.2 Vfwd=1\r\n"
+					+ "+ Vrev=10 Revepsilon=0.2 Epsilon=0.2 Ilimit=7 Revilimit=7)\r\n"
+					+ "A3_0 node[dd] N003_0 DS1\r\n"
+					+ "\r\n"
+					+ "*.model source_diosw sw vt=0 vh=0.001 ron=0.001 roff=10e9\r\n"
+					+ "* Component Conduit{industria:insulated_tin_wire} ConduitPos{A=NodePos{block=[6 -60 -13],node=0},B=NodePos{block=[4 -60 -13],node=1}}\r\n"
+					+ "* Simple resistor for different uses [ r=0.002 ]\r\n"
+					+ "R1_1 node[pos6_-60_-13_id0_lid0_lnml] node[pos4_-60_-13_id1_lid0_lnml] 0.002\r\n"
+					+ "* Simple resistor for different uses [ r=0.002 ]\r\n"
+					+ "R1_2 node[pos6_-60_-13_id0_lid1_lnmn] node[pos4_-60_-13_id1_lid1_lnmn] 0.002\r\n"
+					+ "* Component Block{industria:electro_magnetic_coil} BlockPos{x=4, y=-60, z=-13}\r\n"
+					+ "* Junction resistor\r\n"
+					+ "R1_3 node[pos4_-60_-13_id1_lid0_lnml] node[pos4_-60_-13_id0_lid0_lnml] 0.015\r\n"
+					+ "* Junction resistor\r\n"
+					+ "R1_4 node[pos4_-60_-13_id1_lid1_lnmn] node[pos4_-60_-13_id0_lid1_lnmn] 0.015\r\n"
+					+ "* Load-Resistor with fixed power consumtion [ p_nominal=500.0 ]\r\n"
+					+ "* B1_5 node[pos4_-60_-13_id0_lid0_lnml] node[pos4_-60_-13_id0_lid1_lnmn] P=500.0\r\n"
+					+ "R1_5 node[pos4_-60_-13_id0_lid0_lnml] node[pos4_-60_-13_id0_lid1_lnmn] R=max(V(node[pos4_-60_-13_id0_lid0_lnml], node[pos4_-60_-13_id0_lid1_lnmn])**2/500.0, 0.1)\r\n"
+					+ "\r\n"
+					+ "R0GND node[pos6_-60_-13_id0_lid0_lnml] 0 1\r\n"
+					+ "\r\n"
+					//+ ".op\r\n"
+					+ ".end\r\n"
+					+ "";
+//			String c = "ingame-level-circuit\r\n"
+//					+ "A1 N001 N002 DS1\r\n"
+//					+ ".model DS1 sidiode(Roff=1000 Ron=0.7 Rrev=0.2 Vfwd=1\r\n"
+//					+ "+ Vrev=10 Revepsilon=0.2 Epsilon=0.2 Ilimit=7 Revilimit=7)\n\r"
+//					+ ".end\r\n"
+//					+ "";
+			
+			nglink.loadCircuit(c);
+			
+			//nglink.execCommand("tran 1 1");
+			nglink.execCommand("op");
 			
 			// Native FIXME
 			for (String plot : nglink.listPlots()) {
